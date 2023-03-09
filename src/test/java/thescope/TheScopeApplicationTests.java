704535d81193106cd0c641ca7f25ba03916beb11
Kenny Verheyden <kenny.verheyden@gmail.com>
2023-03-09 10:56:19 +0100

    first commit

---
+++ src/test/java/thescope/TheScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package thescope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
