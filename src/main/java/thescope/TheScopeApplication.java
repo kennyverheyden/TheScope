a73a2b1d4bf1fe1b065c3acc169248123b9ae76d
C04t0 <oscar.yde@gmail.com>
2023-03-13 17:05:24 +0100

    init java-stuff commit

---
+++ src/main/java/thescope/TheScopeApplication.java
@@ -0,0 +1,13 @@
+package thescope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheScopeApplication.class, args);
+	}
+
+}
