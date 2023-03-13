c84dd3d3b8547dcce822074d0c8a62ecdb41889d
C04t0 <oscar.yde@gmail.com>
2023-03-13 19:09:48 +0100

    init java-stuff commit

---
+++ src/main/java/thescope/services/MovieService.java
@@ -0,0 +1,7 @@
+package thescope.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MovieService {
+}
---
+++ src/main/java/thescope/services/UserService.java
@@ -0,0 +1,8 @@
+package thescope.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
