4c889b913e9e6cc7a16b2bc11c7f9ddb38e97be8
Kenny Verheyden <54863392+kennyverheyden@users.noreply.github.com>
2023-03-16 19:19:51 +0100

    Add TheScope.db + update pom.xml + connection test

--- src/main/java/thescope/TheScopeApplication.java
+++ src/main/java/thescope/TheScopeApplication.java
@@ -1,13 +1,35 @@
 package thescope;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import thescope.models.UserRole;
 
 @SpringBootApplication
-public class TheScopeApplication {
+public class TheScopeApplication implements CommandLineRunner {
+
+	//Database connection
+	//Spring Boot will automatically wire this object using application.properties
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheScopeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//Read records:
+		List<UserRole> userRoles = jdbcTemplate.query("SELECT roleName FROM tblUserRole",
+				(resultSet, rowNum) -> new UserRole(resultSet.getString("roleName")));
+		//Print read records:
+		userRoles.forEach(System.out::println);
 	}
 
 }
