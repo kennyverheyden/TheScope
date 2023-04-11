355fbdb4c4dbde9945b2a48e149d01a865d1064d
OlivierBey <116079472+OlivierBey@users.noreply.github.com>
2023-04-11 15:25:45 +0200

    1104

--- src/test/java/thescope/ScheduleShowTest.java
+++ src/test/java/thescope/ScheduleShowTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import thescope.models.ScheduleShow;
 import thescope.services.ScheduleShowService;
 
 @SpringBootTest
@@ -16,7 +18,9 @@ public class ScheduleShowTest {
 	public void contextLoads() {
 		System.out.println("Test successfully loaded");
 	}	
-	
-	
-	
+	@Test
+	public void scheduleshowLoadedTest() {
+	ScheduleShow showtest= sss.findScheduleShowById(1);
+	assertEquals(95, showtest.getMovie().getLength());
+	}
 }
