c704027859753302392d2c8a7104fd58c8f7236c
Kenny Verheyden <54863392+kennyverheyden@users.noreply.github.com>
2023-04-28 20:01:14 +0200

    TheaterRoom - Add room + GUI

--- src/main/java/thescope/controllers/HomeController.java
+++ src/main/java/thescope/controllers/HomeController.java
@@ -48,7 +48,7 @@ public class HomeController {
 		// ***** AUTO LOGIN ADMIN account *****
 		// ***** Development mode *************
 
-		if(doAutoTestLogin(false)) // <-- TRUE or FALSE
+		if(doAutoTestLogin(true)) // <-- TRUE or FALSE
 			return "redirect:/";
 
 		// ***********************************
--- src/main/java/thescope/controllers/TheaterRoomController.java
+++ src/main/java/thescope/controllers/TheaterRoomController.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import thescope.models.TheaterRoom;
 import thescope.services.TheaterRoomService;
@@ -27,4 +30,31 @@ public class TheaterRoomController {
 		model.addAttribute("rooms",rooms);  // map content to html elements
 		return "index";
 	}
+	
+	@PostMapping("/rooms/add") 
+	public String addRoom(@RequestParam (required = false) String location, @RequestParam (required=false) int maxVipSeats, int maxNormalSeats, Model model, RedirectAttributes rm){
+			TheaterRoom room = new TheaterRoom();
+			room.setLocation(location);
+			room.setMaxNormalSeats(maxNormalSeats);
+			room.setMaxVipSeats(maxVipSeats);
+			theaterRoomService.addTheaterRoom(room);
+			model.addAttribute("content", "rooms");
+			// rm.addFlashAttribute("message","Fill in all fields");
+			return "redirect:/rooms";
+		
+	}
+	
+	@PostMapping("/rooms/update") 
+	public String updateRoom(@RequestParam (required = false) String location, @RequestParam (required=false) int maxVipSeats, int maxNormalSeats, Model model, RedirectAttributes rm){
+			
+		
+			// update room 
+		
+		
+			model.addAttribute("content", "rooms");
+			// rm.addFlashAttribute("message","Fill in all fields");
+			return "redirect:/rooms";
+		
+	}
+
 }
